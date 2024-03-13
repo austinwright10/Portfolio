a84f257586962b8f7535ead312ee529685986dda
austinwright10 <austwright10@gmail.com>
2024-03-13 11:44:55 -0600

    started on about me page and fine tuned the picture for different screen sizes

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var descriptionDiv = document.getElementById("description");
+        descriptionDiv.style.opacity = "100";
+    }, 3000);
+});
