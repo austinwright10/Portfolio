6dd3fa019b36ff03402797458e6f622a8b4a6adb
austinwright10 <austwright10@gmail.com>
2024-03-14 17:59:53 -0600

    almost done with the about me section

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var descriptionDiv = document.getElementById("description");
         descriptionDiv.style.opacity = "100";
-    }, 1100);
+    }, 1600);
 });
